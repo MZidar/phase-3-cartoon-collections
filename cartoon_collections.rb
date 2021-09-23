7d29cbf7c27528502737a9d55d4f06532d022cbb
MZidar <MZidar@gmail.com>
2021-09-23 10:28:43 -0400

    commit tree c82d8af22cdafb393f93358b9babd2c4c7afc8ba

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+require 'pry'
+
+
+def roll_call_dwarves(array)
+  array.each.with_index{|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = array.map{|planeteer| planeteer.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  !!array.find{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.intersection(cheese_types) == []
+    return nil
+  else
+    return array.intersection(cheese_types)[0]
+  end
 end
+
+binding.pry
